9d3681238e48b5da26f65b43a4f6397732e740b5
AlexWebUa <alexwebua1@gmail.com>
2020-04-10 22:00:34 +0300

    Made main layout (except the #reviews)

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+let formsData = {};
+
+function collectFormData(form) {
+    let inputs = form.getElementsByTagName('input');
+    for (let i = 0; i < inputs.length; i++) {
+        let name = inputs[i].id;
+        if (inputs[i].type === 'radio') formsData[name] = inputs[i].checked;
+        else formsData[name] = inputs[i].value;
+    }
+}
+
 function sticky_header() {
     if ($(window).width() > 768 && $(window).scrollTop() > 1) {
         if (!$("header").hasClass('header-fixed')) {
@@ -21,4 +32,53 @@ $(window).on('load', () => {
     $(window).on("scroll", function () {
         sticky_header();
     });
+});
+
+$('#quiz1Next').on('click', () => {
+    let form = document.getElementById('quiz1Form');
+    collectFormData(form);
+    $('#quiz1').modal('hide');
+    $('#quiz2').modal('show');
+});
+
+$('#quiz2Next').on('click', () => {
+    let form = document.getElementById('quiz2Form');
+    collectFormData(form);
+    $('#quiz2').modal('hide');
+    $('#quiz3').modal('show');
+});
+
+$('#quiz3Next').on('click', () => {
+    let form = document.getElementById('quiz3Form');
+    collectFormData(form);
+    $('#quiz3').modal('hide');
+    $('#quiz4').modal('show');
+});
+
+
+$('#quiz2Back').on('click', () => {
+    $('#quiz2').modal('hide');
+    $('#quiz1').modal('show');
+});
+
+$('#quiz3Back').on('click', () => {
+    $('#quiz3').modal('hide');
+    $('#quiz2').modal('show');
+});
+
+$('#quiz4Send').on('click', () => {
+    let form = document.getElementById('quiz4Form');
+    collectFormData(form);
+    console.log(formsData);
+});
+
+$(document).on('click', (e) => {
+    if (e.target === $('#dropdown-menu .close')[0])
+        $('#dropdown-menu').css('display', 'none');
+
+    else if (e.target === document.getElementById('dropdown-menu') || e.target === document.getElementById('dropdown-link'))
+        $('#dropdown-menu').css('display', 'flex');
+
+    else
+        $('#dropdown-menu').css('display', 'none');
 });
