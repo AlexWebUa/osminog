5cc72b6a1d97d469b863ce846bb55c5825f506cc
AlexWebUa <alexwebua1@gmail.com>
2020-04-11 22:14:38 +0300

    Tried to add some responsiveness

--- js/main.js
+++ js/main.js
@@ -10,19 +10,28 @@ function collectFormData(form) {
 }
 
 function sticky_header() {
-    if ($(window).width() > 768 && $(window).scrollTop() > 1) {
+    if ($(window).width() >= 768) {
+        if ($(window).scrollTop() > 1) {
+            if (!$("header").hasClass('header-fixed')) {
+                $("header").addClass("header-fixed");
+                $("header .navbar-brand img").attr('src', 'assets/img/logo_white.png');
+                $(".navbar-collapse .icons .instagram-icon a img").attr('src', 'assets/img/instagram_white.png');
+                $(".navbar-collapse .icons .phone-icon a img").attr('src', 'assets/img/phone_white.png');
+            } else return;
+        } else {
+            if ($("header").hasClass('header-fixed')) {
+                $("header").removeClass("header-fixed");
+                $("header .navbar-brand img").attr('src', 'assets/img/logo.png');
+                $(".navbar-collapse .icons .instagram-icon a img").attr('src', 'assets/img/instagram.png');
+                $(".navbar-collapse .icons .phone-icon a img").attr('src', 'assets/img/phone.png');
+            } else return;
+        }
+    } else {
         if (!$("header").hasClass('header-fixed')) {
             $("header").addClass("header-fixed");
             $("header .navbar-brand img").attr('src', 'assets/img/logo_white.png');
             $(".navbar-collapse .icons .instagram-icon a img").attr('src', 'assets/img/instagram_white.png');
             $(".navbar-collapse .icons .phone-icon a img").attr('src', 'assets/img/phone_white.png');
-        } else return;
-    } else {
-        if ($("header").hasClass('header-fixed')) {
-            $("header").removeClass("header-fixed");
-            $("header .navbar-brand img").attr('src', 'assets/img/logo.png');
-            $(".navbar-collapse .icons .instagram-icon a img").attr('src', 'assets/img/instagram.png');
-            $(".navbar-collapse .icons .phone-icon a img").attr('src', 'assets/img/phone.png');
         } else return;
     }
 }
