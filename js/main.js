19affd2cf3bdd7e43915b742e2b0dbe25802a13a
AlexWebUa <alexwebua1@gmail.com>
2020-04-20 23:50:23 +0300

    Final fixes and started services page

--- js/main.js
+++ js/main.js
@@ -15,28 +15,15 @@ function collectFormData(form) {
 }
 
 function sticky_header() {
-    if ($(window).width() >= 768) {
-        if ($(window).scrollTop() > 1) {
-            if (!$("header").hasClass('header-fixed')) {
-                $("header").addClass("header-fixed");
-                $("header .navbar-brand img").attr('src', 'assets/img/logo_white.png');
-                /*$(".navbar-collapse .icons .instagram-icon a img").attr('src', 'assets/img/instagram_white.png');
-                $(".navbar-collapse .icons .phone-icon a img").attr('src', 'assets/img/phone_white.png');*/
-            } else return;
-        } else {
-            if ($("header").hasClass('header-fixed')) {
-                $("header").removeClass("header-fixed");
-                $("header .navbar-brand img").attr('src', 'assets/img/logo.png');
-                /*$(".navbar-collapse .icons .instagram-icon a img").attr('src', 'assets/img/instagram.png');
-                $(".navbar-collapse .icons .phone-icon a img").attr('src', 'assets/img/phone.png');*/
-            } else return;
-        }
-    } else {
+    if ($(window).scrollTop() > 1) {
         if (!$("header").hasClass('header-fixed')) {
             $("header").addClass("header-fixed");
             $("header .navbar-brand img").attr('src', 'assets/img/logo_white.png');
-            /*$(".navbar-collapse .icons .instagram-icon a img").attr('src', 'assets/img/instagram_white.png');
-            $(".navbar-collapse .icons .phone-icon a img").attr('src', 'assets/img/phone_white.png');*/
+        } else return;
+    } else {
+        if ($("header").hasClass('header-fixed')) {
+            $("header").removeClass("header-fixed");
+            $("header .navbar-brand img").attr('src', 'assets/img/logo.png');
         } else return;
     }
 }
@@ -96,7 +83,6 @@ $(document).on('click', (e) => {
     else
         $('#dropdown-menu').css('display', 'none');
 });
-
 
 $(document).ready(() => {
 
@@ -191,3 +177,7 @@ $(document).ready(() => {
         });
     }
 });
+
+
+/* Services page */
+
