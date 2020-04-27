398334b30a4826db796f40f08f02b42456096472
AlexWebUa <alexwebua1@gmail.com>
2020-04-27 13:13:23 +0300

    Finished desktop layout for portfolio page

--- js/portfolio.js
+++ js/portfolio.js
@@ -33,8 +33,50 @@
     }
 });*/
 
-$(".work").on('click touch', e =>{
-    let work = e.currentTarget;
-    let footer = $(work).find('.footer')[0];
-    $(footer).collapse('toggle');
+$(document).ready(()=> {
+    let tabs = $('.tabs button');
+    let works = $('.works .row');
+    let description = $('.work .description');
+    let descriptionHeight = description.outerHeight(true);
+    let title = $(description).find('.title');
+    let titleHeight = title.outerHeight(true);
+    let titleMarginTop = parseInt($('.work .description .title').css('margin-top'));
+    let descriptionOffset = descriptionHeight - titleHeight + titleMarginTop;
+    let workHeight = $('.work .image-wrapper').height() + titleHeight;
+
+    $(tabs).on('click', (e)=>{
+        let activeTab = tabs.closest('.active')[0];
+        let activeCard = works.closest('.active')[0];
+        let targetTab = e.target;
+        let targetCard = $(`#works-${targetTab.id}`)[0];
+        if (activeTab === targetTab) return;
+        activeTab.classList.remove('active');
+        targetTab.classList.add('active');
+
+        animateCSS(`#${activeCard.id}`, 'fadeOut', clearStyles,(el)=>{
+            el.style.display = 'none';
+            activeCard.classList.remove('active');
+            targetCard.classList.add('active');
+            animateCSS(`#${targetCard.id}`, 'fadeIn');
+            targetCard.style = '';
+        })
+
+    })
+
+    $('.work').css('max-height', `${workHeight}px`);
+
+    $('.work').on('click touch', e => {
+        let work = e.currentTarget;
+        let description = $(work).find('.description');
+
+        if($(description).hasClass('expanded') && e.target.tagName !== 'BUTTON') {
+            $(description).css('transform', 'translateY(0px)');
+            $(description).removeClass('expanded');
+        }
+
+        else {
+            $(description).css('transform', `translateY(-${descriptionOffset}px)`);
+            $(description).addClass('expanded');
+        }
+    })
 });
