bc33bea95648c53201cee5e6b2002f70ccc8014e
AlexWebUa <alexwebua1@gmail.com>
2020-05-04 01:43:37 +0300

    Portfolio page adaptation

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,40 +1,6 @@
-/*$(".work").on('click touch', e =>{
-    let work = e.currentTarget;
-
-    if($(work).hasClass('active')) {
-        $(work).removeClass('active');
-        $(work).css('height', '');
-
-        let footer = $(work).find('.footer')[0];
-
-
-        $(footer).css('height', '0px');
-
-        let description = $(work).find('.description')[0];
-        $(description).css('margin-top', '');
-        footer.style = '';
-        $(footer).removeClass('show');
-
-    } else {
-        $(work).addClass('active');
-        let workHeight = $(work).height();
-        $(work).css('height', `${workHeight}px`);
-
-        let description = $(work).find('.description')[0];
-        let footer = $(work).find('.footer')[0];
-
-        $(footer).addClass('show');
-        let footerOuterHeight = $(footer).outerHeight(true);
-        let footerHeight = $(footer).height();
-        $(footer).css('height', `0px`);
-        $(footer).css('height', `${footerHeight}px`);
-
-        $(description).css('margin-top', `-${footerOuterHeight}px`);
-    }
-});*/
-
-$(document).ready(()=> {
-    let tabs = $('.tabs button');
+$(document).ready(() => {
+    let tabs = $('.tabs .tab');
+    if ($(window).width() < 767) tabs = $('.tabs .mobile-only .tab');
     let works = $('.works .row');
     let description = $('.work .description');
     let descriptionHeight = description.outerHeight(true);
@@ -44,7 +10,7 @@ $(document).ready(()=> {
     let descriptionOffset = descriptionHeight - titleHeight + titleMarginTop;
     let workHeight = $('.work .image-wrapper').height() + titleHeight;
 
-    $(tabs).on('click', (e)=>{
+    $(tabs).on('click', (e) => {
         let activeTab = tabs.closest('.active')[0];
         let activeCard = works.closest('.active')[0];
         let targetTab = e.target;
@@ -53,10 +19,23 @@ $(document).ready(()=> {
         activeTab.classList.remove('active');
         targetTab.classList.add('active');
 
-        animateCSS(`#${activeCard.id}`, 'fadeOut', clearStyles,(el)=>{
+        animateCSS(`#${activeCard.id}`, 'fadeOut', clearStyles, (el) => {
             el.style.display = 'none';
             activeCard.classList.remove('active');
+            $(`#${activeCard.id}`).slick('unslick');
             targetCard.classList.add('active');
+            $(`#${activeCard.id}Controls`).css('display', 'none');
+            $(`#${targetCard.id}Controls`).css('display', '');
+            if ($(window).width() < 1200) {
+                $(`#${targetCard.id}`).slick({
+                    infinite: false,
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    prevArrow: $(`#${targetCard.id}Controls .prev`)[0],
+                    nextArrow: $(`#${targetCard.id}Controls .next`)[0],
+                    speed: 1000,
+                });
+            }
             animateCSS(`#${targetCard.id}`, 'fadeIn');
             targetCard.style = '';
         })
@@ -69,14 +48,33 @@ $(document).ready(()=> {
         let work = e.currentTarget;
         let description = $(work).find('.description');
 
-        if($(description).hasClass('expanded') && e.target.tagName !== 'BUTTON') {
+        if ($(description).hasClass('expanded') && e.target.tagName !== 'BUTTON') {
             $(description).css('transform', 'translateY(0px)');
             $(description).removeClass('expanded');
-        }
-
-        else {
+        } else {
             $(description).css('transform', `translateY(-${descriptionOffset}px)`);
             $(description).addClass('expanded');
         }
     })
+
+    if ($(window).width() < 1200) {
+        $(`#works-landing`).slick({
+            infinite: false,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            prevArrow: $(`#works-landingControls .prev`)[0],
+            nextArrow: $(`#works-landingControls .next`)[0],
+            speed: 1000,
+        });
+    }
+    if ($(window).width() < 992) {
+        $('.catalog-list').addClass('collapse');
+        $('.catalog .heading').on('click', ev=>{
+            console.log(ev);
+            // if (!$(ev.target).hasClass('catalog-list')) {
+                $('.catalog-list').collapse('toggle');
+                $('.catalog').toggleClass('showed');
+            // }
+        })
+    }
 });
